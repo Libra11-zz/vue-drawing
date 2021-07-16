730deba01d3b2bc96647e44a6fa024e4736f6043
libra <libra_085925@gmail.com>
2021-07-16 13:55:06 +0800

    change button

--- src/components/kits/export-control.vue
+++ src/components/kits/export-control.vue
@@ -1,15 +1,29 @@
+<!--
+ * @Author: Libra
+ * @Date: 2021-07-16 13:53:26
+ * @LastEditTime: 2021-07-16 13:54:51
+ * @LastEditors: Libra
+ * @Description: 
+ * @FilePath: /vue-drawing/src/components/kits/export-control.vue
+-->
 <template>
   <div class="box">
-    <img src="../../assets/export.png" alt="!">
+    插入
   </div>
 </template>
 
 <script>
   export default {
-    name: 'export-control'
-  }
+    name: "export-control"
+  };
 </script>
 
 <style scoped>
-  @import '../css/kit.css';
+.box {
+  width: 100px;
+  height: 40px;
+  background-color: #dd0000;
+  color: #fff;
+  cursor: pointer;
+}
 </style>
